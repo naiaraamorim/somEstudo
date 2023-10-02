be83319827059ea6eba1dbad56ba0e456a35dac6
Naiara Amorim <naiarataianelimaamorim@gmail.com>
2023-10-02 10:18:49 -0300

    Adicionando pomodoro no site

--- script.js
+++ script.js
@@ -33,3 +33,62 @@ images.forEach(function(image) {
         this.classList.toggle('selected');
     });
 });
+
+const tempo = document.getElementById('tempo');
+const botaoIniciar = document.getElementById('iniciar');
+const botaoPausar = document.getElementById('pause');
+const botaoReiniciar = document.getElementById('reiniciar');
+const alarme = document.getElementById('alarme');
+
+let tempos;
+let minutos = 0;
+let segundos = 5;
+let rodando = false;
+
+function atualizaTempo() {
+    tempo.textContent = `${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
+}
+
+function iniciarTempo() {
+    if (!rodando) {
+        rodando = true;
+        tempos = setInterval(() => {
+            if (minutos === 0 && segundos === 0) {
+                clearInterval(tempos);
+                alarme.play();
+                rodando = false;
+            } else {
+                if (segundos === 0) {
+                    minutos--;
+                    segundos = 59;
+                } else {
+                    segundos--;
+                }
+                atualizaTempo();
+            }
+        }, 1000);
+    }
+}
+
+function pausarTempo() {
+    if (rodando) {
+        clearInterval(tempos);
+        rodando = false;
+    }
+}
+
+function reiniciarTempo() {
+    clearInterval(tempos);
+    rodando = false;
+    minutos = 25;
+    segundos = 0;
+    atualizaTempo();
+    alarme.pause();
+    alarme.currentTime = 0;
+}
+
+botaoIniciar.addEventListener('click', iniciarTempo);
+botaoPausar.addEventListener('click', pausarTempo);
+botaoReiniciar.addEventListener('click', reiniciarTempo);
+
+atualizaTempo();
